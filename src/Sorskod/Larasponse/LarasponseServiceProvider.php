f8b49443dfaf53ab0754fcddace45b90cfe78e4c
Aleksandar Babic <salebab@gmail.com>
2014-09-14 15:46:26 +0200

    tabs replaced with spaces

--- src/Sorskod/Larasponse/LarasponseServiceProvider.php
+++ src/Sorskod/Larasponse/LarasponseServiceProvider.php
@@ -4,32 +4,32 @@ use Illuminate\Support\ServiceProvider;
 
 class LarasponseServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = true;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
         $this->app->bind('League\Fractal\Serializer\SerializerAbstract', 'League\Fractal\Serializer\ArraySerializer');
-		$this->app->bind('Sorskod\Larasponse\Larasponse', 'Sorskod\Larasponse\Providers\Fractal');
-	}
+        $this->app->bind('Sorskod\Larasponse\Larasponse', 'Sorskod\Larasponse\Providers\Fractal');
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('larasponse');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('larasponse');
+    }
 
 }
